e2a79c16a3a6853ba6209d94b2f037a312354f12
developer <developer@finogeeks.com>
2023-10-29 16:57:54 +0800

    initial commit

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <finclipsdk/finclipsdk_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FinclipsdkPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FinclipsdkPluginCApi"));
+}
